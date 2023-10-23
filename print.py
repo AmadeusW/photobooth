b7eddbab959ec75c0c17c3192f6453d4f4c6572c
Amadeusz Wieczorek <git@amadeusw.com>
2023-10-23 16:50:59 -0700

    print fixup

--- print.py
+++ print.py
@@ -2,4 +2,4 @@ import subprocess
 
 class PrinterAdapter:
     def print(self, directory, filename):
-        subprocess.call(["lp", "-d Brother_HL_L2350DW_series", f"{directory}/{filename}"])
+        subprocess.call(["lp", f"-d Brother_HL_L2350DW_series {directory}/{filename}"])
