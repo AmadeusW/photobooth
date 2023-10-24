1fbbfa3f362556771ff59f1693fa9cf8e8075375
Amadeusz Wieczorek <git@amadeusw.com>
2023-10-23 20:14:29 -0700

    fit to page

--- print.py
+++ print.py
@@ -2,4 +2,4 @@ import subprocess
 
 class PrinterAdapter:
     def print(self, directory, filename):
-        subprocess.call(["lp", "-d", "Brother_HL_L2350DW_series", f"{directory}/{filename}"])
+        subprocess.call(["lp", "-d", "Brother_HL_L2350DW_series", "-o", "fit-to-page", f"{directory}/{filename}"])
