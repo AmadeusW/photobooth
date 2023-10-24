8609c927bad64237f33cc42a8ae8a29281a17d16
Amadeusz Wieczorek <git@amadeusw.com>
2023-10-23 20:54:38 -0700

    print params

--- print.py
+++ print.py
@@ -2,4 +2,4 @@ import subprocess
 
 class PrinterAdapter:
     def print(self, path):
-        subprocess.call(["lp", "-d", "Brother_HL_L2350DW_series", "-o", "fit-to-page", f"{path}"])
+        subprocess.call(["lp", "-d", "Brother_HL_L2350DW_series", "-o", "landscape", "-o", "ppi=180", f"{path}"])
