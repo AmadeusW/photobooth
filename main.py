2783a54b77753b2ec9492971935ad3be62ccd45d
Amadeusz Wieczorek <git@amadeusw.com>
2023-11-17 23:17:32 -0800

    fix gpio

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ from gpiozero import LED, Button
 from signal import pause
 import photobooth
 
-# Define the GPIO pin number you want to use
 gpioLed = 4
 gpioButton = 14
-
-# Create an LED object for the specified GPIO pin
 led = LED(gpioLed)
 button = Button(gpioButton)
-
-led.on()
-button.when_pressed = onPress
 
 def onPress():
     led.off()
     photobooth.takePhoto()
     led.on()
 
+led.on()
+button.when_pressed = onPress
 pause()
